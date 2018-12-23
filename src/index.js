e88c0f51848d39446e9cbabbbe0b4621dfdedf00
Natasha <natashakhema@gmail.com>
2018-12-23 22:29:16 +1100

    add semantic ui css stylesheet

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+    return (
+        <div>
+            <h1>hello, my name is nat</h1>
+        </div>
+
+    )
+
+};
+
+ReactDOM.render(
+<App />,
+document.getElementById("root")
+)
