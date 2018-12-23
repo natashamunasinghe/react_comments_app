061ce06716503c26609292caa6a2f0e589f801dc
Natasha <natashakhema@gmail.com>
2018-12-23 22:51:18 +1100

    add faker images

--- src/index.js
+++ src/index.js
@@ -1,17 +1,61 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import faker from 'faker';
 
+//example of non-reuseable JSX
 const App = () => {
     return (
-        <div>
-            <h1>hello, my name is nat</h1>
+        <div className="ui container comments">
+            <div className="comment">
+                <a href="/" className="avatar">
+                    <img alt="avatar" src={faker.image.avatar()} />
+                </a>
+                <div className="content">
+                    <a href="/" className="author">
+                    Sam
+                    </a>
+                    <div className="metadata">
+                        <span className="date"> Today at 6pm</span>
+                    </div>
+                    <div className="text">Nice blog post!
+                    </div>
+                </div>
+            </div>
+            <div className="comment">
+                <a href="/" className="avatar">
+                    <img alt="avatar" src={faker.image.avatar()} />
+                </a>
+                <div className="content">
+                    <a href="/" className="author">
+                    Sam
+                    </a>
+                    <div className="metadata">
+                        <span className="date"> Today at 6pm</span>
+                    </div>
+                    <div className="text">Nice blog post!
+                    </div>
+                </div>
+            </div>
+            <div className="comment">
+                <a href="/" className="avatar">
+                    <img alt="avatar" src={faker.image.avatar()} />
+                </a>
+                <div className="content">
+                    <a href="/" className="author">
+                    Sam
+                    </a>
+                    <div className="metadata">
+                        <span className="date"> Today at 6pm</span>
+                    </div>
+                    <div className="text">Nice blog post!
+                    </div>
+                </div>
+            </div>
         </div>
-
-    )
-
+    );       
 };
 
 ReactDOM.render(
 <App />,
 document.getElementById("root")
-)
+);
