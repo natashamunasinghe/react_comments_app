daaa83743f30632528b25c9c4d34133273eced00
Natasha <natashakhema@gmail.com>
2018-12-28 17:05:34 +1100

    add props children

--- src/index.js
+++ src/index.js
@@ -2,31 +2,40 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import CommentDetail from "./CommentDetail";
 import faker from "faker";
+import ApprovalCard from "./ApprovalCard";
 
 //example of showing 1 component within another
 const App = () => {
     return (
         <div className="ui container comments">
-           <CommentDetail 
-           author="Sam"
-           date="12th June"
-           text="Nice blog!"
-           avatar={faker.image.avatar()}
-           />
-           <CommentDetail 
-           author="Jane"
-           date="14th Feb"
-           text="Lost interest!"
-           avatar={faker.image.avatar()}
+            <ApprovalCard>
+                <CommentDetail 
+                author="Sam"
+                date="12th June"
+                text="Nice blog!"
+                avatar={faker.image.avatar()}
+                />
+           </ApprovalCard>
 
-           />  
-           <CommentDetail 
-           author="Alex"
-           date="1st Dec"
-           text="Learnt lots - thanks"
-           avatar={faker.image.avatar()}
+           <ApprovalCard>
+                <CommentDetail 
+                author="Jane"
+                date="14th Feb"
+                text="Lost interest!"
+                avatar={faker.image.avatar()}
 
-           />     
+                />  
+           </ApprovalCard>
+
+           <ApprovalCard>
+                <CommentDetail 
+                author="Alex"
+                date="1st Dec"
+                text="Learnt lots - thanks"
+                avatar={faker.image.avatar()}
+
+                />
+           </ApprovalCard>     
         </div>
     );       
 };
